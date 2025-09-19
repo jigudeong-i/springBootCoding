08fa55b3da5469edc9619f41b6731c79e3bd99bb
jigudeong_i <wlrnejddl@naver.com>
2025-09-19 17:54:00 +0900

    no message

---
+++ webSiteReactProject/src/main/java/com/spring/mallapi/dto/MemberDTO.java
@@ -0,0 +1,49 @@
+package com.spring.mallapi.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ToString
+@Getter
+@Setter
+public class MemberDTO extends User {    //User:사용자 인증 권한 정보를 담는 클래스
+    private static final long serialVersionUID = 1L;
+
+    private String email;
+    private String pw;
+    private String nickname;
+    private boolean social;
+    private List<String> roleNames = new ArrayList<>();
+
+    public MemberDTO(String email, String pw, String nickname, boolean social, List<String> roleNames) {
+        // SimpleGrantedAuthority: Spring Security의 권한 클래스 - ROLE_ 접두어를 붙여 권한으로 등록 (ROLE_USER, ROLE_ADMIN)
+        super(email, pw, roleNames.stream().map(str -> new SimpleGrantedAuthority("ROLE_" + str)).collect(Collectors.toList()));
+        this.email = email;
+        this.pw = pw;
+        this.nickname = nickname;
+        this.social = social;
+        this.roleNames = roleNames;
+    }
+
+    public Map<String, Object> getClaims() {
+        Map<String, Object> dataMap = new HashMap<>();
+
+        dataMap.put("email", email);
+        dataMap.put("pw", pw);
+        dataMap.put("nickname", nickname);
+        dataMap.put("social", social);
+        dataMap.put("roleNames", roleNames);
+
+        return dataMap;
+    }
+}
